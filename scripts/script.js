de6eac4181921c91e3d543b19fb0cc0061c7d9e7
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2023-12-01 16:03:42 +0100

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -33,7 +33,10 @@ const meerLezenWaarom = document.getElementById('meer_lezen_waarom');
 
 meerLezenWaarom.addEventListener('click', function () {
     meerLezenHiddenWaarom.classList.toggle('show');
+    meerLezenWaarom.innerHTML = meerLezenWaarom.innerHTML === 'Minder lezen' ? 'Meer lezen' : 'Minder lezen';
 });
+
+
 function showUpButton() {
     var upButton = document.getElementById("up_button");
     if (upButton) {
@@ -76,4 +79,44 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
     window.addEventListener('scroll', handleScroll);
+});
+
+document.getElementById('empty').addEventListener('click', function() {
+    const image1 = document.getElementById('img1');
+    const image2 = document.getElementById('img2');
+  
+    if (image2.style.opacity === '1') {
+      image2.style.opacity = '0';
+      image1.style.opacity = '1';
+    } else {
+      image2.style.opacity = '1';
+      image1.style.opacity = '0';
+    }
+  });
+
+document.getElementById('btn1').addEventListener('click', function() {
+    const p1 = document.getElementById('p1');
+
+    if (window.getComputedStyle(p1).getPropertyValue('opacity') === '1') {
+        p1.style.opacity = '0';
+        
+    } else {
+        p1.style.opacity = '1';
+        setTimeout(function() {
+            p1.style.opacity = '0';
+        }, 5000);
+    }
+});
+
+document.getElementById('btn2').addEventListener('click', function() {
+    const p1 = document.getElementById('p2');
+
+    if (window.getComputedStyle(p2).getPropertyValue('opacity') === '1') {
+        p2.style.opacity = '0';
+    } else {
+        p2.style.opacity = '1';
+        setTimeout(function() {
+            p1.style.opacity = '0';
+        }, 5000);
+    }
 });
